e02aa6d15eb207c46185b495337a5b3606e25efc
Ranjith <160145715+Ranjithkumar-Srinivasan@users.noreply.github.com>
2026-04-22 14:49:31 +0530

    Update app.js

--- node-app/app.js
+++ node-app/app.js
@@ -1,12 +1,9 @@
 const http = require('http');
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Node.js App Running 🚀</h1><p>Output from Jenkins Pipeline Success!</p>');
+    res.end('<h1>Success! Node.js Output is Live 🚀</h1>');
 });
-
-// Port 3000 is standard for Node.js
 server.listen(3000, '0.0.0.0', () => {
-    console.log('Server running at http://98.93.50.93:3000/');
+    console.log('Server is running...');
 });
